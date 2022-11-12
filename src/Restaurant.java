cc942ad7bb5688437d175cadbca6c96c8616f520
Cathal Crowe <cathalcrowepatrick@gmail.com>
2022-11-12 18:43:59 +0000

    Added the restaurant class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,5 @@
 public class Restaurant {
     public static void main(String[] args) {
-        System.out.println("This is a restaurant");
+        System.out.println("this is a restaurant!");
     }
 }
