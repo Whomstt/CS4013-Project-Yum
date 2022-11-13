955b3752818e3368a4f6720302c44d3be84802aa
Cathal Crowe <cathalcrowepatrick@gmail.com>
2022-11-13 19:22:40 +0000

    Added baba boey

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,10 @@
 public class Restaurant {
+
+    //Constructor for a restaurant
+    public Restaurant() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("this is a restaurant!");
+        System.out.println("baba boey");
     }
 }
