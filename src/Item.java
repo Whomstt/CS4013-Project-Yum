ac1b3f90369fd40c839a24ff6ea4682f8e09388a
Robert <robertflan02@gmail.com>
2022-11-14 19:18:19 +0000

    Removed some unnecessary methods

--- src/Item.java
+++ src/Item.java
@@ -15,25 +15,15 @@ public class Item {
     }
 
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public String toString() {
         return name + " €" + price;
