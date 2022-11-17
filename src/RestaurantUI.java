6aa19994042185290977c1cf2131e6a161da60c3
Cathal <cathalcrowepatrick@gmail.com>
2022-11-17 17:55:14 +0000

    reservations mini change

--- src/RestaurantUI.java
+++ src/RestaurantUI.java
@@ -1,19 +1,19 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RestaurantUI {
+    private int reservationID = 1;
     private ArrayList<Reservation> reservations = new ArrayList<Reservation>();
     private Scanner in;
-    public RestaurantUI() {
+    public RestaurantUI() throws FileNotFoundException {
         in = new Scanner(System.in);
     }
     public void run(Restaurant restaurant) throws IOException {
         boolean more = true;
         while(more) {
-            System.out.println("(P)lace reservation  (S)how Reservations  (Q)uit");
+            System.out.println("(P)lace reservation  (S)how Reservations  (R)emove Reservation  (Q)uit");
             String command = in.nextLine().toUpperCase();
             if (command.equals("P")) {
                 System.out.println("Enter Reservation Name:");
@@ -26,8 +26,9 @@ public class RestaurantUI {
                 String numberOfPeople = in.nextLine();
                 System.out.println("Enter phone number:");
                 String phoneNumber = in.nextLine();
-                Reservation reservation = new Reservation(name, day, time, numberOfPeople, phoneNumber);
+                Reservation reservation = new Reservation(reservationID, name, day, time, numberOfPeople, phoneNumber);
                 reservations.add(reservation);
+                reservationID++;
             } else if (command.equals("Q")) {
                 System.out.println("Exiting program... \uD83C\uDF46");
                 more = false;
@@ -40,6 +41,9 @@ public class RestaurantUI {
                 }
                 System.out.println();
                 sc.close();
+            } else if (command.equals("R")) {
+                System.out.println("Enter Reservation ID of booking you would like to cancel");
+
             }
         }
     }
